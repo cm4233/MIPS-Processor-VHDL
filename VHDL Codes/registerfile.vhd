c3421d612cce6bf414cabbd1cb044144fc8e5f57
Chandra Kiran Mettu <cm4233@nyu.edu>
2018-04-15 22:48:59 -0700

    Update registerfile.vhd

--- VHDL Codes/registerfile.vhd
+++ VHDL Codes/registerfile.vhd
@@ -4,25 +4,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity registerfile is
----------//ports//---------
+---------//ports//----------------------------------------------------------------------
 PORT (
-A1: in STD_LOGIC_VECTOR(4 DOWNTO 0);
-A2: in STD_LOGIC_VECTOR(4 DOWNTO 0);
-A3: in STD_LOGIC_VECTOR(4 DOWNTO 0);
-WD3: in STD_LOGIC_VECTOR(31 DOWNTO 0);
-WE3: in STD_LOGIC;
-clk: in STD_LOGIC;
+A1: in STD_LOGIC_VECTOR(4 DOWNTO 0);--addr port 1 to read reg, reg content goes to RD1
+A2: in STD_LOGIC_VECTOR(4 DOWNTO 0);--addr port 2 to read reg, reg content goes to RD2
+A3: in STD_LOGIC_VECTOR(4 DOWNTO 0);--addr port 3 to write data in WD3 into register 
+WD3: in STD_LOGIC_VECTOR(31 DOWNTO 0);--data to be written into reg specified by A3
+WE3: in STD_LOGIC;--write enable for A3 and WD3
+clk: in STD_LOGIC;--clock
 
-RD1: out STD_LOGIC_VECTOR(31 DOWNTO 0);
-RD2: out STD_LOGIC_VECTOR(31 DOWNTO 0);
+RD1: out STD_LOGIC_VECTOR(31 DOWNTO 0);--shows reg content of the reg specified by A1 
+RD2: out STD_LOGIC_VECTOR(31 DOWNTO 0);--shows reg content of the reg specified by A2 
+-----------------------------------------------------------------------------------------
 
-R0:out STD_LOGIC_VECTOR(31 DOWNTO 0);
-R1:out STD_LOGIC_VECTOR(31 DOWNTO 0);
-R2:out STD_LOGIC_VECTOR(31 DOWNTO 0);
-R3:out STD_LOGIC_VECTOR(31 DOWNTO 0);
+---------------//additional testing ports//-----------------------------------------------
+R0:out STD_LOGIC_VECTOR(31 DOWNTO 0);--Probing additional signals directly to these 
+R1:out STD_LOGIC_VECTOR(31 DOWNTO 0);--ports to directly see and check the the register content
+R2:out STD_LOGIC_VECTOR(31 DOWNTO 0);--of a few registers in simulation instead of using   
+R3:out STD_LOGIC_VECTOR(31 DOWNTO 0);--a load instruction for each individual register
 R4:out STD_LOGIC_VECTOR(31 DOWNTO 0);
-R5:out STD_LOGIC_VECTOR(31 DOWNTO 0);
-R6:out STD_LOGIC_VECTOR(31 DOWNTO 0);
+R5:out STD_LOGIC_VECTOR(31 DOWNTO 0);--Probed only regs R0-R22 beacuse they were beign used
+R6:out STD_LOGIC_VECTOR(31 DOWNTO 0);--in our test programs
 R7:out STD_LOGIC_VECTOR(31 DOWNTO 0);
 R8:out STD_LOGIC_VECTOR(31 DOWNTO 0);
 R9:out STD_LOGIC_VECTOR(31 DOWNTO 0);
@@ -46,8 +48,11 @@ end registerfile;
 
 architecture Behavioral of registerfile is
 -----//signals//----------
+--32x32 ram is the reg file 
+--so totally 32 registers and each reg is 32bit wide
+--since 5 bits were allocated to the regs in the ISA, we have 32 regs
 TYPE ram IS ARRAY (0 TO 31) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
-SIGNAL regfile: 
+SIGNAL regfile:                                               
 ram:=ram'(x"00000000", x"00000000", x"00000000", x"00000000",
 x"00000000", x"00000000", x"00000000", x"00000000",
 x"00000000", x"00000000", x"00000000", x"00000000",
@@ -59,24 +64,25 @@ x"00000000", x"00000000", x"00000000", x"00000000");
 ---------------------------
 begin
 ----------//code//---------
-RD1<=regfile(CONV_INTEGER(A1));
-RD2<=regfile(CONV_INTEGER(A2));
+RD1<=regfile(CONV_INTEGER(A1));--read data from reg specified by addr A1
+RD2<=regfile(CONV_INTEGER(A2));--read data from reg specified by addr A2
 process(clk)
 begin
 if (clk'EVENT AND clk='1') then
-if(WE3='1')then
-regfile(CONV_INTEGER(A3))<=WD3;
+if(WE3='1')then                 --write enable
+regfile(CONV_INTEGER(A3))<=WD3; --write data in WD3 into reg specified by addr A3
 end if;
 end if;
 end process;
 
-R0<=regfile(0);
-R1<=regfile(1);
-R2<=regfile(2);
-R3<=regfile(3);
+-----//additional testing ports//--------
+R0<=regfile(0);--Probing additional signals directly to these
+R1<=regfile(1);--ports to directly see and check the the register content
+R2<=regfile(2);--of a few registers in simulation instead of using
+R3<=regfile(3);--a load instruction for each individual register
 R4<=regfile(4);
-R5<=regfile(5);
-R6<=regfile(6);
+R5<=regfile(5);--Probed only regs R0-R22 beacuse they were beign used
+R6<=regfile(6);--in our test programs
 R7<=regfile(7);
 R8<=regfile(8);
 R9<=regfile(9);
