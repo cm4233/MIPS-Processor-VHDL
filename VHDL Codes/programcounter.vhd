a23ead5d5040d36b1e4f8790f33e44915364763f
Chandra Kiran Mettu <cm4233@nyu.edu>
2018-04-15 23:09:51 -0700

    Update programcounter.vhd

--- VHDL Codes/programcounter.vhd
+++ VHDL Codes/programcounter.vhd
@@ -6,7 +6,7 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity programcounter is
 ---------//ports//---------
 PORT (
-EorD: in STD_LOGIC;
+EorD: in STD_LOGIC;--this signal is for our test program to either encrypt or decrypt first
 din: in STD_LOGIC_VECTOR(31 DOWNTO 0);
 clk: in STD_LOGIC;
 clr: in STD_LOGIC;
@@ -18,11 +18,11 @@ architecture Behavioral of programcounter is
 begin
 ----------//code//---------
 PROCESS (clr, clk)  BEGIN
-  IF (clr='0' and EorD='0') THEN dout <= x"00000000";
-  ELSIF (clr='0' and EorD='1') THEN dout <= x"0000006E";
-  ELSIF (clk'EVENT AND clk='1') THEN dout<=din;
-  END IF;
-END PROCESS;
+  IF (clr='0' and EorD='0') THEN dout <= x"00000000"; --start PC from here to start encryption for our test program inside IM
+  ELSIF (clr='0' and EorD='1') THEN dout <= x"0000006E";-- PC start from here to do decryption for our test program inside the IM 
+  ELSIF (clk'EVENT AND clk='1') THEN dout<=din;-- The ProgCounter here is just a flip flop whose output will go into the addr port 
+  END IF;                                      -- of the IM. It's input comes from the top module and CU which decides whether to pass
+END PROCESS;                                   -- "PC+1" or "PC+branchOffset" to the program counter depending on the current instr. 
 --------------------------
 end Behavioral;
 
